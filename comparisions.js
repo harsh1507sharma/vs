7028e8ec289f84a6d5fd61a7c766592b18d9aa6d
HARSH SHARMA <hs0797227@gmail.com>
2025-06-24 19:53:39 +0530

    comparision of datatypes

---
+++ comparisions.js
@@ -0,0 +1,14 @@
+console.log(null==0)
+console.log(null>0)
+console.log(null<=0)
+// comaparions null ko value de dete h isiliye third wala case true h and equality comapriosn check mein nahi aata hai
+
+console.log(undefined==0)
+
+console.log(undefined>0)
+
+console.log(undefined>=0)
+
+// === isse unke daatatype tak bhi check ho jaate hai 
+console.log("2"==2)
+console.log("2"===2)
