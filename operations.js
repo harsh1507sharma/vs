8923ea9217ab87a5e50c419aba84f90410fb78db
HARSH SHARMA <hs0797227@gmail.com>
2025-06-24 19:27:22 +0530

    basic operations

---
+++ operations.js
@@ -0,0 +1,22 @@
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/3)
+console.log(2%2)
+ let str1 = "diya"
+let str2 = " and harsh"
+let str3 = str1 + str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2 +2)
+//string first mein hai to sabko string ki trah treat kiya jayega 
+console.log(1+2 +"2")
+// string last  mein h to pehle first wala operation solve kiya jayega
+let gamecounter =100
+let y = ++gamecounter
+let z = gamecounter++
+console.log(y)
+console.log(z)
+// for more explaination do chstgpt
